e6ac5495fccf31163be4b31a57b3e2949939d894
vladimir <vladimirmuratov@mail.ru>
2024-04-03 18:15:16 +0300

    update services

--- src/components/base/Accordion/BaseAccordion.jsx
+++ src/components/base/Accordion/BaseAccordion.jsx
@@ -1,26 +1,19 @@
 import {Box} from '@mui/material'
 import {BaseAccordionItem} from '@/components/base/Accordion/BaseAccordionItem'
-import {useState} from 'react'
 
 export const BaseAccordion = ({id = '', items = []}) => {
-    const [expanded, setExpanded] = useState('')
-
-    const handleChange = (panel) => {
-        setExpanded(panel)
-    }
 
     return (
         <Box
             id={id}
             sx={{
-            scrollMarginTop: {xs: '120px', sm: '50px'},
-            paddingX: {xs: '12px', sm: '18px', md: '22px'},
-            paddingY: {xs: '25px', sm: '50px'},
-            color: 'var(--black)'
-        }}>
+                scrollMarginTop: {xs: '120px', sm: '50px'},
+                paddingX: {xs: '12px', sm: '18px', md: '22px'},
+                paddingY: {xs: '25px', sm: '50px'},
+                color: 'var(--black)'
+            }}>
 
-            {items.map((item) => <BaseAccordionItem key={item.id} {...item} expanded={expanded}
-                                                    onChange={handleChange}/>)}
+            {items.map((item) => <BaseAccordionItem key={item.id} {...item}/>)}
 
         </Box>
     )
--- src/components/base/Accordion/BaseAccordionItem.jsx
+++ src/components/base/Accordion/BaseAccordionItem.jsx
@@ -1,17 +1,17 @@
 import {Accordion, AccordionDetails, AccordionSummary, Typography} from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
-export const BaseAccordionItem = ({id, question, answer, expanded, onChange}) => {
+export const BaseAccordionItem = ({id, question, answer}) => {
 
     return(
-        <Accordion expanded={expanded === `panel${id}`} onChange={() => onChange(`panel${id}`)}>
+        <Accordion>
             <AccordionSummary
                 sx={{fontWeight: 500, fontSize: 18}}
                 expandIcon={<ExpandMoreIcon/>}
                 aria-controls={`panel${id}-content`}
                 id={`panel${id}-header`}
             >
-                <Typography sx={{fontSize: {sm: '18px', md: '20px'}}}>{question}</Typography>
+                <Typography sx={{fontSize: {sm: '18px', md: '20px'}, ':hover': {color: 'var(--blue)'}, transition: 'all 0.3s'}}>{question}</Typography>
             </AccordionSummary>
             <AccordionDetails sx={{backgroundColor: 'var(--blue)', color: 'var(--white)', whiteSpace: 'pre-wrap'}}>
                 <Typography sx={{fontSize: {sm: '18px', md: '20px'}, lineHeight: 1.5}}>{answer}</Typography>
