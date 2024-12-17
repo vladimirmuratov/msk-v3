117d261e6710a59a11131481962d92fa6929c3ac
vladimir <vladimirmuratov@mail.ru>
2024-12-17 19:11:21 +0300

    update h1 banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import { Box, Button, Typography } from '@mui/material';
 import { phoneNumber } from '@/config';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 export const Banner = () => {
     const router = useRouter();
-    const [isMobile, setMobile] = useState(false);
+    /*const [isMobile, setMobile] = useState(false);
 
     useEffect(() => {
         const os = navigator.userAgentData.platform;
@@ -14,35 +14,42 @@ export const Banner = () => {
             setMobile(true);
         }
 
-    }, []);
+    }, []);*/
 
     return (
-        <Box sx={{ position: 'relative' }}>
-            <img className="banner" src="/images/banner.jpg" alt="banner" style={{ filter: 'brightness(65%)' }} />
+        <Box sx={{
+            position: 'relative',
+            backgroundImage: 'url(/images/banner.jpg)',
+            height: '80vh',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            // filter: 'brightness(65%)'
+        }}>
+            {/*<img className="banner" src="/images/banner.jpg" alt="banner" style={{ filter: 'brightness(65%)' }} />*/}
             <Box sx={{
                 position: 'absolute',
                 top: '20%',
                 left: '5%',
                 display: 'flex',
                 flexDirection: 'column',
-                gap: { xs: '10px', sm: '15px' }
+                gap: { xs: '10px', sm: '15px' },
             }}>
                 <Typography
                     variant="h1"
                     sx={{
                         fontSize: { xs: '36px', sm: '48px', md: '64px' },
                         fontWeight: 500,
-                        lineHeight: 1,
+                        // lineHeight: 1,
                         color: 'var(--white)',
-                        textShadow: '0 2px var(--black)',
+                        textShadow: '0 2px 2px var(--black)',
                         textTransform: 'uppercase',
-                        textAlign: 'center'
+                        textAlign: 'center',
                     }}>Лечение в лучших клиниках москвы</Typography>
                 <Typography
                     variant="h2"
                     sx={{
                         fontSize: { xs: '18px', sm: '28px', md: '32px' },
-                        lineHeight: 1,
+                        // lineHeight: 1,
                         color: 'var(--white)',
                         textShadow: '0 2px var(--black)',
                         textAlign: 'center'
